594f2231fa91cac60735d2cb568ed60100ccd990
曹鈞翔 <caojunxiang@caojunxiangdeMacBook-Pro.local>
2022-05-23 20:34:42 +0800

    First commit in IntelliJ

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("hello world");
+        Person tsao = new Person("Tsao");
+        System.out.println(tsao.getName());
+
+    }
+}
